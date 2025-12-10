71f750ae1809599c071ee685b53eba0c82185984
JayRaj21 <jrus08@yahoo.com>
2025-12-10 15:41:08 -0500

    finished data preprocessing

--- ML Project.py
+++ ML Project.py
@@ -1,130 +1,133 @@
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-import sklearn.mixture as mix
+from sklearn.cluster import KMeans
+from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
+from datetime import datetime
+from collections import defaultdict
 
 """
-Potential Classification Rules:
+This is is a classification problem. In a broad context, 
+I am trying to identify spending patterns. I want to identify 
+groups of high spending and predict
 
-Group by year
+Problem statement:
 
-Group by year and month
+What is my input: 
 
-Group by month only
+What is my output:
 
-Group by week
+Identify the best train/test split for the data: 60/40, 70/30, 80/20
+Use accuracy score and decision boundary plots to determine best fit
 
-Group by day of the week
+input:[[mean spending, variance, prob of class n]]
 
-Group amount by some threshold
-    Below 10
-    Between 10 and 20
-    Between 20 and 30
-    Between 30 and 40
-    ...
+labels: a vector a labels corresponding to the input data
 
-Things worth considering 
-    Applying PCA to the data before performing 
-    any clustering or classification
+output: 
 
-Decision rule:
-    Amount based rule
-    y < 15
-
-    15 < y < 30
-
-    30 < y < 60
-
-    60 < y < 80
-
-    y > 80
-
-    Next steps:
-    Figure out a good number of class labels to use for my decision rule
-    Figure out how to identiy probability for each category on each day
-    Try using mean + c * std decision rule
-
-    Consider identifying the likilhood of a specific category being purchased on a given week
-    Consider classifying based on food types, healthiness, etc.
+Data processing:
+Manually editing csv data to include valid columns and combine redundant data entries
 
 """
 
 #interpret and process the data
-RAW_DATA = pd.read_csv("credit_transactions.csv")
-TRANSACT_DATA = RAW_DATA[RAW_DATA["Amount"]<=0].copy()
+# RAW_DATA = pd.read_csv("credit_transactions.csv")
+# RAW_DATA = pd.read_csv("Credit_Transactions_Cleaned.csv")
+RAW_DATA = pd.read_csv("Credit_Transactions_Cleaned_v2.csv")
+TRANSACT_DATA = RAW_DATA[RAW_DATA["Amount"] <= 0].copy()
 TRANSACT_DATA["Amount"] = TRANSACT_DATA["Amount"] * -1
-groupedTransacts = [[], [], [], [], [], [], []]
-weekNum, amountList = list(), list()
 
-#organizes the data set into groups of days in a week
-for i in TRANSACT_DATA["Effective Date"]:
-    weekNum.append(dt.datetime.strptime(i, '%m/%d/%Y').isoweekday())
+categorizedTransacts = dict()
+categorizedDates = dict()
+for i in range(len(TRANSACT_DATA["Amount"])):
+    amount = TRANSACT_DATA["Amount"][i]
+    date = TRANSACT_DATA["Effective Date"][i]
+    description = TRANSACT_DATA["Extended Description"][i]
+    if description not in categorizedTransacts.keys():
+        categorizedTransacts[description], categorizedDates[description] = list(), list()
+        categorizedTransacts[description].append(amount)
+        categorizedDates[description].append(date)
 
-for i in TRANSACT_DATA["Amount"]:
-    amountList.append(i)
+    else:
+        categorizedTransacts[description].append(amount)
+        categorizedDates[description].append(date)
 
-for i in range(len(weekNum)):
-    groupedTransacts[weekNum[i] - 1].append(amountList[i])
+for i, j in categorizedDates.items():
+    categorizedDates[i] = [datetime.strptime(d, "%m/%d/%Y") for d in j]
 
+groupedPairs = dict() 
+datesByWeek = dict()
+transacByWeek = dict()
+
+for category in categorizedDates.keys():
+    categDates = categorizedDates[category]
+    categTransac = categorizedTransacts[category]
+
+    weeklyPairs = defaultdict(list)
+    weeklyDates = defaultdict(list)
+    weeklyTransac = defaultdict(list)
+
+    for date, transac in zip(categDates, categTransac):
+        y, w, _ = date.isocalendar()
+        key = (y, w)
+
+        weeklyPairs[key].append((date, transac))
+
+        weeklyDates[key].append(date)
+        weeklyTransac[key].append(transac)
+
+    groupedPairs[category] = dict(weeklyPairs)
+    datesByWeek[category] = dict(weeklyDates)
+    transacByWeek[category] = dict(weeklyTransac)
+
+weeklyMeans = dict()
+
+for category, weeks in transacByWeek.items():
+    weeklyMeans[category] = {}
+    for key, tx_list in weeks.items():
+        weeklyMeans[category][key] = [np.mean(tx_list), np.var(tx_list)]
+
+
+inputData = []
+for i, j in weeklyMeans.items():
+    temp = []
+    for k, v in j.items():
+        # print(f"{k}, {v}, {i}")
+        inputData.append([float(v[0]), float(v[1]), i])
+
+#frequency based decision rule
+categoryFreq = dict()
+for i in TRANSACT_DATA["Extended Description"]:
+    if i in categoryFreq.keys():
+        categoryFreq[i]+=1
+
+    else:
+        categoryFreq[i] = 1
+
+totalPurchases = sum(categoryFreq.values())
+classRules = dict()
+for i in categoryFreq.keys():
+    classRules[i] = (categoryFreq[i])/totalPurchases
+
+# for i, j in categoryFreq.items():
+#     print(f"{i}: {j}")
+
+
+# amount based decision rule
 #make the data continuous by its date so that it can be given class labels
 CONTIN_DATA = TRANSACT_DATA.copy()
 CONTIN_DATA["Effective Date"] = pd.to_datetime(CONTIN_DATA["Effective Date"])
-CONTIN_DATA = CONTIN_DATA.groupby('Effective Date')['Amount'].sum().to_frame()
-full_index = pd.date_range(CONTIN_DATA.index.min(), CONTIN_DATA.index.max(), freq='D')
-CONTIN_DATA = CONTIN_DATA.reindex(full_index).fillna(0)
-CONTIN_DATA['X'] = (CONTIN_DATA.index - CONTIN_DATA.index.min()).days
-X, y = CONTIN_DATA[['X']].values, CONTIN_DATA['Amount'].values
-LABELS = list()
-
-# assigns relevant class labels according to transaction amount
-for transac in y:
-    if transac < 15:
-        LABELS.append(1.0)
-
-    elif transac >= 15 and transac < 30:
-        LABELS.append(2.0)
-
-    elif transac >= 30 and transac < 60:
-        LABELS.append(3.0)
-
-    elif transac >= 60 and transac < 80:
-        LABELS.append(4.0)
-    
-    else:
-        LABELS.append(5.0)
-
-processedData2 = []
-for i in range(X.shape[0]):
-    processedData2.append([X[i][0].item(), y[i].item(), LABELS[i]])
-
-CLASSIFY_DATA = np.array(processedData2)
-
-
-# for i in allData:
-#     print(i)
-# print(RAW_DATA.info())
-# print(TRANSACT_DATA["Extended Description"])
-
-# TRANSACT_DATA['Amount'].plot(kind='hist', bins=50, edgecolor='black', alpha=0.7)
-# plt.title('Transactions Distribution')
-# plt.xlabel('Amount')
-# plt.ylabel('Frequency')
-
-# TRANSACT_DATA.plot(kind="scatter", color="g", x="Effective Date", y="Amount")
-# plt.title('Transactions for a year')
-# plt.xlabel('Effective Date')
-# plt.ylabel('Amount')
-
-# plt.hist(classDict.values(), bins=len(classDict), color="g")
-# plt.title("Number of transactions per category")
-# plt.ylabel("Frequency")
-# plt.xlabel("Spending Categories")
-# plt.show()
+CONTIN_DATA['week'] = CONTIN_DATA["Effective Date"].dt.to_period('W')
+weeklyTransactions = CONTIN_DATA.groupby('week')['Amount'].apply(list).to_dict()
+normalizedTransactions = dict()
+for i, j in weeklyTransactions.items():
+    normalizedTransactions[i] = [np.mean(j), np.var(j)]
 
 def padData(data):
     maxLen = 0
@@ -143,100 +146,28 @@ def padData(data):
 
     return data
 
-#function for calculating and graphing the k-means-clustering solution
-#params: data (np.array), k (int)
-#return: errorSum (np.array), members (np.array), centers (np.array)
-def kMeansClustering(data, k, plotCheck = False):
-    centers = np.random.rand(k, data.shape[-1]) #randomly generated initial cluster center
-    distArr = np.full([data.shape[0], k], 0)    #distance matrix
-    members = np.full([data.shape[0], k], 0)    #matrix of different clusters
-    errorSum = 0                                #sum-of-squares error
-    prevError = np.inf                          #previous error value
-    iter = 0                                    #iteration variable for plotting
 
-    #initiliazes distance matrix with randomly generated cluster center and data point
-    for i in range(data.shape[0]):
-        for j in range(k):
-            distArr[i, j] = np.linalg.norm(centers[j, :] - data[i, :])
-            # print(distArr[i, j])
+def plotKMeans(k, init, iter, rand):
+    model = KMeans(n_clusters=k, n_init=init, max_iter=iter, random_state=rand)
+    model.fit()
+    predictor = model.predict()
 
-    #runs E and M step for 200 iterations or until the error reaches a set amount (1e-6)
-    for i in range(200):
-        #E-Step
-        iter = i
-        for p in range(data.shape[0]):
-            members[p, :] = np.zeros(k)  #resetting cluster matrix
-            minVal = np.argmin(distArr[p, :])   #finding closest value to cluster centers
-            members[p, minVal] = 1  #assiging point to new cluster
-            print(minVal, members[p,: ])
-
-        #M-Step
-        for c in range(centers.shape[0]):
-            # print(centers[c])
-            print(members[:, c])
-            # print(np.argwhere(members[:, c] == 1))
-            avg = np.mean(data[np.argwhere(members[:, c] == 1)], axis = 0)
-            # print(avg)
-            
-            
-            # note that this code needs to be fixed but is working for now
-            validAvg = True
-            for a in avg:
-                if a.all():
-                    pass
-                else:
-                   validAvg = False 
-                   break
-            if not validAvg:
-                centers[c] = avg
-            else:
-                centers[c] = 0
-
-        for p in range(data.shape[0]):
-            for q in range(k):
-                # print(centers[q,:])
-                # print(data[p,:])
-                # print(centers[q,:]-data[p,:])
-                # print(np.linalg.norm(centers[q,:]-data[p,:]))
-                distArr[p, q] = np.linalg.norm(centers[q, :] - data[p, :])
-        
-        #computes the sum-of-squares error value
-        prevError = errorSum
-        errorSum = 0
-        for p in range(data.shape[0]):
-            for q in range(k):
-                if (members[p, q] == 1):
-                    sqr = (distArr[p, q])**2
-                    errorSum += sqr
-        
-        #stops iterating if the error threshold has been reached
-        if (np.abs(prevError - errorSum) < 1e-6): break
-
-        #plots the graph for the initial, and first iterations
-        if plotCheck and (i == 0 or i == 1): plotClusters(data, k, errorSum, members, centers, i)
-    
-    #plots clustered data for the final iteration
-    if plotCheck: plotClusters(data, k, errorSum, members, centers, iter)
-    return errorSum, members, centers
-
-def plotClusters(data, k, error, members, centers, iter):
-    plt.figure()
-    for i in range(members.shape[-1]):
-        points = np.squeeze(data[np.argwhere(members[:, i] == 1)])
-        plt.scatter(points[:, 0], points[:, 1])
-        plt.scatter(centers[i,0], centers[i, 1], marker = "X", color = "black")
-    plt.title(f"Iteration {iter}, k = {k} --> Error = {error}")
+    for i in range(predictor.shape[0]):
+        plt.scatter(x=predictor[i, 0], y = predictor[i, 1], marker = "x", color = "r")
+      
+    plt.title("K Means Clustering")
     plt.xlabel("x data")
     plt.ylabel("y data")
     plt.show()
     return
 
+
 def plotSVM(data):
-    xData, yData = data[:, 0:2], data[:, 2]
+    inputData, labels = data[:, 0:2], data[:, 2]
 
     #computing using linear kernal
     linearModel = svm.SVC(kernel = "linear")
-    linearModel.fit(xData, yData)
+    linearModel.fit(inputData, labels)
     linPredict = linearModel.predict(data[:, 0:2])
 
     for i in range(linPredict.shape[0]):
@@ -247,7 +178,7 @@ def plotSVM(data):
             plt.scatter(x=data[i, 0], y = data[i, 1], marker = "o", color = "g")
 
         else:
-            plt.scatter(x=data[i, 0], y = data[i, 1], marker = "*", color = "m")
+            plt.scatter(x=data[i, 0], y = data[i, 1], marker = "*", color = "b")
       
     plt.title("SVM with linear kernal")
     plt.xlabel("x data")
@@ -256,7 +187,7 @@ def plotSVM(data):
 
     #computing using radial basis function kernal
     rbfModel = svm.SVC(kernel = "rbf")
-    rbfModel.fit(xData, yData)
+    rbfModel.fit(inputData, labels)
     rbfPredict = rbfModel.predict(data[:, 0:2])
 
     for i in range(rbfPredict.shape[0]):
@@ -267,7 +198,7 @@ def plotSVM(data):
             plt.scatter(x=data[i, 0], y = data[i, 1], marker = "o", color = "g")
 
         else:
-            plt.scatter(x=data[i, 0], y = data[i, 1], marker = "*", color = "m")
+            plt.scatter(x=data[i, 0], y = data[i, 1], marker = "*", color = "b")
     
     plt.title("SVM with Radial Kernal")
     plt.xlabel("x data")
@@ -280,9 +211,9 @@ def plotGMM(data):
 
 
 def plotLogReg(data):
-    xData, yData = data[:, 0:2], data[:, 2]
+    inputData, labels = data[:, 0:2], data[:, 2]
     logModel = LogisticRegression()
-    logModel.fit(xData, yData)
+    logModel.fit(inputData, labels)
     logPredict = logModel.predict(data[:, 0:2])
     
     #plots the original data points with their actual classifications
@@ -308,17 +239,16 @@ def plotLogReg(data):
     plt.xlabel("x data")
     plt.ylabel("y data")
     plt.show()
-    plt.show()
     
     #calculating cross-entropy loss and number of points missclassified
     yPred = logModel.predict_proba(data[:, 0:2])
     ceLoss = 0
     
     for i in range(yPred.shape[0]):
-        ceLoss += np.log(yPred[i,int(yData[i]-1)])
+        ceLoss += np.log(yPred[i,int(labels[i]-1)])
     
     ceLoss *= -1
-    classAcc = logModel.score(data[:, 0:2],yData)
+    classAcc = logModel.score(data[:, 0:2],labels)
 
     return ceLoss, (1 - classAcc)
 
@@ -340,11 +270,17 @@ def plotDescBound(model):
                 plt.scatter(x=x[i,j], y=y[i,j], color = "b", alpha = 0.1, marker = "o")
     return
 
+def plotDecisionTree(data):
+    return
+
+
+def plotRandForest(data):
+    return
+
 
 if __name__ == "__main__":
     pass
-    data = np.array(padData(groupedTransacts))
-    # kMeansClustering(data, 5, False)
-    plotSVM(CLASSIFY_DATA)
-    # plotLogReg(CLASSIFY_DATA)
+    data = np.array(inputData)
+    plotSVM(data)
+    # plotLogReg(data)
     
